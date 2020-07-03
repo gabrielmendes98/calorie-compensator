35cc526ad10a0ab261cebdf5a4a46fca1c144b4e
Gabriel <gabrielmssantiago@gmail.com>
2020-07-03 18:49:59 -0300

    componentes home criados - falta estilizar

---
+++ src/pages/Home/Explanation/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import './styles.css';
+
+const Explanation: React.FC = ({ children }) => {
+  return <div className="explanation">{children}</div>;
+};
+
+export default Explanation;
--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,9 +1,45 @@
 import React from 'react';
 
+import Explanation from './Explanation';
+
+import fries from '../../assets/fries.png';
+import hamburger from '../../assets/hamburger.png';
+import coke from '../../assets/coke.png';
+import chicken from '../../assets/chicken.png';
+import weightMachine from '../../assets/weight-machine.png';
+import exercise1 from '../../assets/exercise1.svg';
+import exercise2 from '../../assets/exercise2.svg';
+import exercise3 from '../../assets/exercise3.svg';
+
+import './styles.css';
+
 const Home = () => {
   return (
     <div id="home">
-      <h1>Home</h1>
+      <h1>Welcome to the Calorie Compensator.</h1>
+      <h2>How it works?</h2>
+      <Explanation>
+        <div>
+          <img src={fries} alt="fries" />
+          <img src={hamburger} alt="hamburger" />
+          <img src={coke} alt="coke" />
+          <img src={chicken} alt="chicken" />
+        </div>
+        <p>1. Choose the food you want to eat.</p>
+      </Explanation>
+      <Explanation>
+        <img src={weightMachine} alt="weightMachine" />
+        <p>2. Enter your current weight.</p>
+      </Explanation>
+      <Explanation>
+        <div>
+          <img className="exercise" src={exercise1} alt="weightMachine" />
+          <img className="exercise" src={exercise2} alt="weightMachine" />
+          <img className="exercise" src={exercise3} alt="weightMachine" />
+        </div>
+        <p>3. Ready! We will give you a list of exercises that you can practice to burn the calories ingested.</p>
+      </Explanation>
+      <button>Start now!</button>
     </div>
   );
 };
