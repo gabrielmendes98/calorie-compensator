341e3e725fd04406b26c86a385617d5389fcd9bb
Gabriel <gabrielmssantiago@gmail.com>
2020-07-08 17:22:29 -0300

    funcao passada para o componente pai

--- src/pages/Search/Input/index.tsx
+++ src/pages/Search/Input/index.tsx
@@ -1,34 +1,10 @@
-import React, { useEffect, useState } from 'react';
+import React, { InputHTMLAttributes } from 'react';
 
 import { FiSearch } from 'react-icons/fi';
 
-import api from '../../../services/api';
-
 import './styles.css';
 
-interface Food {
-  description: string;
-  brandOwner: string;
-}
-
-const Input = () => {
-  const [foodName, setFoodName] = useState('');
-  const [foodList, setFoodList] = useState([]);
-
-  useEffect(() => {
-    if (foodName !== '') {
-      const timeout = setTimeout(async () => {
-        const response = await api.get(`foods/search?api_key=${process.env.REACT_APP_API_KEY}&query=${foodName}`);
-        const foods = response.data.foods;
-        const filteredFoods = foods.map((food: Food) => ({ name: food.description, brand: food.brandOwner }));
-        setFoodList(filteredFoods);
-        console.log(filteredFoods);
-      }, 2000);
-
-      return () => clearTimeout(timeout);
-    }
-  }, [foodName]);
-
+const Input: React.FC<InputHTMLAttributes<any>> = ({ value, onChange }) => {
   return (
     <div className="search-input">
       <FiSearch />
@@ -37,8 +13,8 @@ const Input = () => {
         name="search"
         id="search"
         placeholder="Enter a food or brand name"
-        value={foodName}
-        onChange={(e) => setFoodName(e.target.value)}
+        value={value}
+        onChange={onChange}
       />
     </div>
   );
--- src/pages/Search/index.tsx
+++ src/pages/Search/index.tsx
@@ -1,11 +1,35 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 
 import BackButton from '../../components/BackButton';
 import Input from './Input';
 
+import api from '../../services/api';
+
 import './styles.css';
 
+interface Food {
+  description: string;
+  brandOwner: string;
+}
+
 const Search = () => {
+  const [foodName, setFoodName] = useState('');
+  const [foodList, setFoodList] = useState([]);
+
+  useEffect(() => {
+    if (foodName !== '') {
+      const timeout = setTimeout(async () => {
+        const response = await api.get(`foods/search?api_key=${process.env.REACT_APP_API_KEY}&query=${foodName}`);
+        const foods = response.data.foods;
+        const filteredFoods = foods.map((food: Food) => ({ name: food.description, brand: food.brandOwner }));
+        setFoodList(filteredFoods);
+        console.log(filteredFoods);
+      }, 2000);
+
+      return () => clearTimeout(timeout);
+    }
+  }, [foodName]);
+
   return (
     <div id="search-page">
       <div className="header">
@@ -13,7 +37,7 @@ const Search = () => {
         <BackButton />
       </div>
       <div className="search">
-        <Input />
+        <Input value={foodName} onChange={(e) => setFoodName(e.target.value)} />
         <button>Search</button>
       </div>
     </div>
