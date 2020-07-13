e60492f8cc1ea078ff8b84a5e3d0695c5220e728
Gabriel <gabrielmssantiago@gmail.com>
2020-07-13 19:00:57 -0300

    set-weight navigation done

--- src/pages/SetWeight/index.tsx
+++ src/pages/SetWeight/index.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { RouteComponentProps, Link } from 'react-router-dom';
 
 import { FiArrowRight } from 'react-icons/fi';
 
@@ -7,17 +7,27 @@ import BackButton from '../../components/BackButton';
 
 import './styles.css';
 
-const SetWeight = () => {
+const SetWeight: React.FC<RouteComponentProps> = ({ location }) => {
+  const [weight, setWeight] = useState(0);
+
   return (
     <div id="set-weight">
       <BackButton />
       <div className="container">
         <h2>2. Enter your current weight in kg.</h2>
         <div className="weight-input">
-          <input type="number" name="weight" id="weight" />
+          <input
+            type="number"
+            name="weight"
+            id="weight"
+            value={weight}
+            onChange={(e) => setWeight(Number(e.target.value))}
+          />
           <span>KG</span>
         </div>
-        <Link to={`/`}>
+        <Link
+          to={`/compensation-info?calories=${new URLSearchParams(location.search).get('calories')}&weight=${weight}`}
+        >
           <span>Next</span>
           <div>
             <FiArrowRight />
