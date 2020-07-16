584014efa3e8b7a1a7f313976f879ef606eee37e
Gabriel <gabrielmssantiago@gmail.com>
2020-07-16 16:47:35 -0300

    renderizando na tabela

--- src/pages/Compensation/index.tsx
+++ src/pages/Compensation/index.tsx
@@ -1,15 +1,34 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { RouteComponentProps } from 'react-router-dom';
 
 import { FiDownload } from 'react-icons/fi';
 
 import BackButton from '../../components/BackButton';
+import activityValues from '../../data/activity-values.json';
 
 import './styles.css';
 
+interface Activity {
+  activity: string;
+  description: string;
+  duration: number;
+}
+
 const Compensation: React.FC<RouteComponentProps> = ({ location }) => {
+  const [activities, setActivities] = useState<Activity[]>([]);
+
   const calories = new URLSearchParams(location.search).get('calories');
   const weight = new URLSearchParams(location.search).get('weight');
+
+  useEffect(() => {
+    const _activities = activityValues.map(({ activity, description, met }) => {
+      const duration = (200 * Number(calories)) / (3.5 * met * Number(weight));
+
+      return { activity, description, duration };
+    });
+
+    setActivities(_activities);
+  }, [calories, weight]);
 
   return (
     <div id="compensation">
@@ -25,6 +44,22 @@ const Compensation: React.FC<RouteComponentProps> = ({ location }) => {
           </div>
         </button>
         <BackButton />
+      </div>
+      <div className="container">
+        <table>
+          <tr>
+            <th>Activity</th>
+            <th>Description</th>
+            <th>Duration in minutes</th>
+          </tr>
+          {activities.map((activityItem) => (
+            <tr>
+              <td>{activityItem.activity}</td>
+              <td>{activityItem.description}</td>
+              <td>{Math.round(activityItem.duration)}</td>
+            </tr>
+          ))}
+        </table>
       </div>
     </div>
   );
