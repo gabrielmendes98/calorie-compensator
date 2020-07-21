4dab032a8918bd650bfcc0637eb5546dd525f0aa
Gabriel <gabrielmssantiago@gmail.com>
2020-07-21 15:41:07 -0300

    scroll top quando navega

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import { BrowserRouter } from 'react-router-dom';
 
 import Routes from './routes';
 import Footer from './components/Footer';
+import ScrollToTop from './components/ScrollToTop';
 
 import './App.css';
 
 function App() {
   return (
     <BrowserRouter>
+      <ScrollToTop />
       <main>
         <Routes />
       </main>
